8b752b7bdc125a72a3051e286be47dc39bfb6724
LKW00 <133477728+LKW00@users.noreply.github.com>
2025-04-03 19:59:12 +0900

    Add files via upload

---
+++ 백준 2839번.cpp
@@ -0,0 +1,25 @@
+//#include<iostream>
+//
+//using namespace std;
+//
+//int main() {
+//	int a;
+//	int num=0;
+//	cin >> a;
+//
+//	while (a%5!=0) {
+//		a-=3;
+//		num++;
+//		if (a < 0) {
+//			cout << -1;
+//			return 0;
+//		}
+//		if (a == 0) {
+//			cout << num;
+//			return 0;
+//		}
+//	}
+//	num += a / 5;
+//	cout<<num;
+//	return 0;
+//}
