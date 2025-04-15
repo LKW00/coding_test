4f02a1575314993afd428d4a7816c7a90842f4e6
LKW00 <133477728+LKW00@users.noreply.github.com>
2025-04-16 02:13:44 +0900

    Add files via upload

---
+++ 백준 10986번.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+long long arr[1000001];
+int main() {
+	int N, M;
+	cin >> N >> M;
+	long long sum = 0;
+	long long count = 0;
+	vector<int> v(M,0);
+	for (int i = 0;i < N;i++) {
+		long long num;
+		cin >> num;
+		sum += num%M;
+		arr[i] = sum%M;
+	}
+	for (int i = 0;i < N;i++) {
+		v[arr[i]]++;
+	}
+	for (int i = 0;i < M;i++) {
+		if (i == 0)
+			count += v[i] * (v[i] + 1) / 2;
+		else 
+			count += v[i] * (v[i] - 1) / 2;
+	}
+	cout << count;
+
+}
