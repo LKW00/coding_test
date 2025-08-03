cf5d7e04f23d21b8efa55c3cad650ffc55e99341
LKW00 <133477728+LKW00@users.noreply.github.com>
2025-08-03 21:18:38 +0900

    우선 순위 큐

---
+++ 백준 1927번.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false); // 입출력 속도 향상
+    cin.tie(nullptr);
+
+    int N;
+    cin >> N;
+
+    priority_queue<int, vector<int>, greater<int>> minHeap;
+
+    for (int i = 0; i < N; ++i) {
+        int x;
+        cin >> x;
+
+        if (x == 0) {
+            if (minHeap.empty()) {
+                cout << "0\n";
+            }
+            else {
+                cout << minHeap.top() << '\n';
+                minHeap.pop();
+            }
+        }
+        else {
+            minHeap.push(x);
+        }
+    }
+
+    return 0;
+}
