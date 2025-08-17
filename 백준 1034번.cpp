12358345592c34b82c648f7dab802c97bf1ca2df
LKW00 <133477728+LKW00@users.noreply.github.com>
2025-08-17 21:43:15 +0900

    백준 1034번.cpp

--- 백준 1034번.cpp
+++ 백준 1034번.cpp
@@ -1,29 +1,31 @@
 #include <iostream>
 #include <vector>
-
+#include <string>
+#include <algorithm>
 using namespace std;
-long long arr[1000001];
-int main() {
-        int N, M;
-        cin >> N >> M;
-        long long sum = 0;
-        long long count = 0;
-        vector<int> v(M,0);
-        for (int i = 0;i < N;i++) {
-                long long num;
-                cin >> num;
-                sum += num%M;
-                arr[i] = sum%M;
-        }
-        for (int i = 0;i < N;i++) {
-                v[arr[i]]++;
-        }
-        for (int i = 0;i < M;i++) {
-                if (i == 0)
-                        count += v[i] * (v[i] + 1) / 2;
-                else 
-                        count += v[i] * (v[i] - 1) / 2;
-        }
-        cout << count;
 
-}//수정
+int main() {
+    int N, M;
+    cin >> N >> M;
+    vector<string> table(N);
+    for (int i = 0; i < N; i++) {
+        cin >> table[i];
+    }
+    int K;
+    cin >> K;
+
+    int answer = 0;
+    for (int i = 0; i < N; i++) {
+        int zero_cnt = count(table[i].begin(), table[i].end(), '0');
+        if (zero_cnt <= K && (K - zero_cnt) % 2 == 0) {
+            int cnt = 0;
+            for (int j = 0; j < N; j++) {
+                if (table[i] == table[j]) cnt++;
+            }
+            answer = max(answer, cnt);
+        }
+    }
+
+    cout << answer << "\n";
+    return 0;
+}
