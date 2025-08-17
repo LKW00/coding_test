af222ce718485fd8870f0a02909cba6d14ca9451
LKW00 <133477728+LKW00@users.noreply.github.com>
2025-08-17 21:44:25 +0900

    백준 1032번.cpp

--- 백준 1032번.cpp
+++ 백준 1032번.cpp
@@ -1,28 +1,25 @@
-#include <iostream>
-#include <vector>
-#include <string>
+#include<iostream>
+#include<vector>
 using namespace std;
 
 int main() {
-    int N;
-    cin >> N;
-    vector<string> files(N);
-
-    for (int i = 0; i < N; i++) {
-        cin >> files[i];
-    }
-
-    string pattern = files[0]; // 첫 번째 파일명으로 시작
-
-    for (int col = 0; col < pattern.size(); col++) {
-        for (int row = 1; row < N; row++) {
-            if (files[row][col] != files[0][col]) {
-                pattern[col] = '?'; // 다른 문자가 있으면 ? 처리
-                break;
-            }
-        }
-    }
-
-    cout << pattern << "\n";
-    return 0;
+	int N;
+	cin >> N;
+	vector<string> v(N);
+	string a;
+	for (int i = 0;i < N;i++) {
+		cin >> v[i];
+	}
+	for (int i = 0;i < v[0].size();i++) {
+		for (int j = 0;j < N;j++) {
+			if (v[0][i] != v[j][i]) {
+				a += '?';
+				break;
+			}
+			if (j == N - 1) {
+				a += v[0][i];
+			}
+		}
+	}
+	cout << a;
 }
