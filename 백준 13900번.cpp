8006405c18582a236811ca3f23480dd3d99af51b
LKW00 <133477728+LKW00@users.noreply.github.com>
2025-09-12 16:26:27 +0900

    누적 합

--- 백준 13900번.cpp
+++ 백준 13900번.cpp
@@ -1,21 +1,22 @@
-//#include <iostream>
-//#include<vector>
-//using namespace std;
-//
-//int main() {
-//	long long a;
-//	cin >> a;
-//	vector<int>v(a);
-//	long long result = 0;
-//	for (int i = 0;i < a;i++) {
-//		cin >> v[i];
-//	}
-//	long long sum=0;
-//	for (int i = 0;i < a;i++) {
-//		sum += v[i];
-//	}
-//	for (int i = 0;i < a;i++) {
-//		result+=(sum - v[i])* v[i];
-//	}
-//	cout << result/2;
-//}
+#include <iostream>
+#include<vector>
+using namespace std;
+
+int main() {
+	long long a;
+	cin >> a;
+	vector<int>v(a);
+	long long result = 0;
+	for (int i = 0;i < a;i++) {
+		cin >> v[i];
+	}
+	long long sum=0;
+	for (int i = 0;i < a;i++) {
+		sum += v[i];
+	}
+	for (int i = 0;i < a;i++) {
+		result+=(sum - v[i])* v[i];
+	}
+	cout << result/2;
+
+}
