2b6a31cc5aa5f4af2cf535c054d267db349e974c
LKW00 <133477728+LKW00@users.noreply.github.com>
2025-09-12 16:24:22 +0900

    누적 합

---
+++ 백준 16139번.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include<map>
+using namespace std;
+
+int main() {
+	string S;
+	cin >> S;
+	int q;
+	cin >> q;
+	vector<vector<int>>v(26,vector<int>(S.length()));
+	vector<int>result;
+	v[S[0] - 'a'][0]++;
+	for (int i = 1;i < S.length();i++) {
+		
+		for (int j = 0;j < 26;j++) {
+			if (v[j][i - 1] > 0) {
+				v[j][i] = v[j][i - 1];
+			}
+		}
+		v[S[i] - 'a'][i]++;
+	}
+	for (int i = 0;i < q;i++) {
+		char a;
+		cin >> a;
+		int l, r;
+		cin >> l >> r;
+		if (l > 0)
+			result.push_back(v[a - 'a'][r] - v[a - 'a'][l - 1]);
+		else
+			result.push_back(v[a - 'a'][r]);
+	}
+	for (int i = 0;i < q;i++) {
+		cout << result[i]<<'\n';
+	}
+}
